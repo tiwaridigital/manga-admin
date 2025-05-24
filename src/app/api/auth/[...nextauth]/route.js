fc529c1c6f2aad96942f064c343e33ebd6f0f813
Ujjwal Kumar <ujwltiwari494@gmail.com>
2025-05-24 17:56:49 +0530

    updates

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -3,7 +3,13 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 import GoogleProvider from 'next-auth/providers/credentials';
 
 const handler = NextAuth({
-  ...
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+  ],
 });
 
 export { handler as GET, handler as POST };
